268b97d0a26c172ec8033150fa20fb1c87a2b51d
SharmarAndrey <sharnarandrey@gmail.com>
2025-02-21 00:40:35 +0100

    Integrate MongoDB into the Express server using Mongoose.

--- backend/index.js
+++ backend/index.js
@@ -20,14 +20,5 @@ app.listen(PORT, () => {
 });
 
 // Connect to MongoDB
-// mongoose
-// 	.connect(process.env.MONGODB_URI, {
-// 		useNewUrlParser: true,
-// 		useUnifiedTopology: true,
-// 	})
-// 	.then(() => {
-// 		console.log("Connected to MongoDB");
-// 	})
-// 	.catch((error) => {
-// 		console.error("Error connecting to MongoDB:", error);
-// 	});
+const connectDB = require("./db");
+connectDB();
