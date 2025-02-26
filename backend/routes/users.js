ebe20dcc681958eb4a3cd30510b11fe931fb5a85
SharmarAndrey <sharmarandrey@gmail.com>
2025-02-26 01:27:07 +0100

    add hashedPassword for user with bcryptjs.

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
 // User registrations)
 router.post("/", async (req, res) => {
@@ -14,12 +15,20 @@ router.post("/", async (req, res) => {
         .status(400)
         .json({ error: "User with this email already exists" });
     }
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     // We create a new user
-    const user = new User({ name, email, password, role });
+    const user = new User({ name, email, password: hashedPassword, role });
     await user.save();
 
-    res.status(201).json(user);
+    res
+      .status(201)
+      .json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
