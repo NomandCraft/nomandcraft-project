3c7c31098df7800d24d385b32476e5c62b6cbf6a
SharmarAndrey <sharmarandrey@gmail.com>
2025-02-28 00:07:19 +0100

    middleware for centralized error handling.

--- backend/index.js
+++ backend/index.js
@@ -4,31 +4,39 @@ const cors = require("cors");
 /* const mongoose = require("mongoose"); */
 const morgan = require("morgan");
 
-const app = express();
-app.use(express.json());
-app.use(morgan("dev"));
-app.use(cors());
-
-//  Routes
-app.get("/health", (req, res) => {
-  res.json({ status: "ok" });
-});
+// Connect to MongoDB
+const connectDB = require("./db");
+connectDB();
 
 // Connect routes
 require("./models/user");
 require("./models/camper");
 
+//  Health Check Route
+app.get("/health", (req, res) => {
+  res.json({ status: "ok" });
+});
+
+// Connect routes
 const camperRoutes = require("./routes/campers");
 app.use("/api/campers", camperRoutes);
 
 const userRoutes = require("./routes/users");
 app.use("/api/users", userRoutes);
 
+const app = express();
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
+
+//middleware for centralized error handling.
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: err.message || "Internal Server Error" });
+  next(err);
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-// Connect to MongoDB
-const connectDB = require("./db");
-connectDB();
