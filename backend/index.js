f2eac8a26dff3f9dfd3a4334f9d675f699061fe9
SharmarAndrey <sharmarandrey@gmail.com>
2025-03-04 00:31:19 +0100

    Change port for bacjand to 5000

--- backend/index.js
+++ backend/index.js
@@ -36,7 +36,7 @@ app.use((err, req, res, next) => {
   next(err);
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
