4efc1c7fec696b4f42dbd90f5afef33259c1413c
SharmarAndrey <sharmarandrey@gmail.com>
2025-03-08 16:43:13 +0100

    refractoring db,js

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,11 +4,11 @@ const connectDB = async () => {
   if (mongoose.connection.readyState >= 1) return;
 
   try {
+    console.log('MongoDB URI:', process.env.MONGODB_URI);
     await mongoose.connect(process.env.MONGODB_URI);
     console.log('✅ MongoDB connected');
   } catch (error) {
     console.error('❌ MongoDB connection error:', error);
-    process.exit(1);
   }
 };
 
