cc13bfa753e335a54833c88fc28a04a729c6232a
SharmarAndrey <sharmarandrey@gmail.com>
2025-03-20 02:06:29 +0100

    implementation of burger-menu

---
+++ frontend/src/components/Layout/BurgerMenu.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="relative">
+    <!-- Анимированная кнопка бургер-меню -->
+    <button
+      @click="menuOpen = !menuOpen"
+      class="flex flex-col justify-center items-center w-10 h-10 focus:outline-none"
+    >
+      <span
+        class="block w-8 h-1 bg-gray-700 dark:bg-white rounded transition-all duration-300"
+        :class="{ 'rotate-45 translate-y-2': menuOpen }"
+      ></span>
+      <span
+        class="block w-8 h-1 bg-gray-700 dark:bg-white rounded transition-all duration-300 my-1"
+        :class="{ 'opacity-0': menuOpen }"
+      ></span>
+      <span
+        class="block w-8 h-1 bg-gray-700 dark:bg-white rounded transition-all duration-300"
+        :class="{ '-rotate-45 -translate-y-2': menuOpen }"
+      ></span>
+    </button>
+
+    <!-- Выпадающее меню -->
+    <div
+      v-if="menuOpen"
+      class="absolute top-12 right-0 bg-white dark:bg-gray-800 shadow-lg rounded w-48 p-4 transition-all duration-300 transform origin-top"
+    >
+      <ul class="space-y-4 font-medium">
+        <router-link class="nav-link block" to="/" @click="menuOpen = false">HOME</router-link>
+        <router-link class="nav-link block" to="/campers" @click="menuOpen = false"
+          >CAMPERVANS</router-link
+        >
+        <router-link class="nav-link block" to="/about" @click="menuOpen = false"
+          >ABOUT OUR VANS</router-link
+        >
+        <router-link class="nav-link block" to="/categories" @click="menuOpen = false"
+          >CATEGORIES</router-link
+        >
+        <router-link class="nav-link block" to="/gallery" @click="menuOpen = false"
+          >GALLERY</router-link
+        >
+        <router-link class="nav-link block" to="/blogs" @click="menuOpen = false"
+          >BLOGS</router-link
+        >
+        <router-link class="nav-link block" to="/contact" @click="menuOpen = false"
+          >CONTACT</router-link
+        >
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const menuOpen = ref(false)
+</script>
+
+<style scoped>
+.nav-link {
+  @apply text-gray-700 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400;
+}
+</style>
--- frontend/src/components/Layout/NavBar/NavBar.vue
+++ frontend/src/components/Layout/NavBar/NavBar.vue
@@ -2,14 +2,33 @@
   <nav class="bg-white dark:bg-gray-900 border-b border-gray-200">
     <div class="container mx-auto flex justify-between items-center p-4">
       <NavLogo />
-      <NavLinks />
+      <!--  <!-Burger-Men (appears at the width <1280px)-> -->
+      <BurgerMenu v-if="!isDesktop" />
+      <NavLinks v-if="isDesktop" />
       <UserMenu />
     </div>
   </nav>
 </template>
 
 <script setup>
+import { ref, onMounted, onUnmounted } from 'vue'
 import NavLogo from './NavLogo.vue'
 import NavLinks from './NavLinks.vue'
 import UserMenu from './UserMenu.vue'
+import BurgerMenu from '../BurgerMenu.vue'
+
+const isDesktop = ref(window.innerWidth >= 1280)
+
+// Follow the change in the width of the screen
+const updateScreenSize = () => {
+  isDesktop.value = window.innerWidth >= 1280
+}
+
+onMounted(() => {
+  window.addEventListener('resize', updateScreenSize)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('resize', updateScreenSize)
+})
 </script>
