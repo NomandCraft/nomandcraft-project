19820b95199c61f477273e6a94247036e9b69251
SharmarAndrey <sharmarandrey@gmail.com>
2025-03-20 01:45:12 +0100

    фвфмешму background

--- frontend/src/components/Layout/DefaultLayout.vue
+++ frontend/src/components/Layout/DefaultLayout.vue
@@ -1,12 +1,35 @@
 <template>
-  <div class="layout flex flex-col min-h-screen">
+  <div class="layout flex flex-col custom-bg w-full h-screen">
     <NavBar />
-    <main class="flex-1 container mx-auto p-4">
+    <main class="flex-1 flex flex-col justify-between container mx-auto p-4">
       <slot />
     </main>
     <FooterComponent />
   </div>
 </template>
+
+<style scoped>
+.custom-bg {
+  background-image: url('/src/assets/images/bg_image_VW2.jpg');
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: contain;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+
+@media (min-width: 768px) {
+  .custom-bg {
+    background-size: cover !important;
+  }
+}
+@media (max-width: 768px) {
+  .custom-bg {
+    background-position: top;
+  }
+}
+</style>
 
 <script setup>
 /* import NavBar from './navbar/NavBar.vue' */
