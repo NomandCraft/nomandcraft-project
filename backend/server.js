d4515f38283753614a857f2b501f99cfe325b655
SharmarAndrey <sharmarandrey@gmail.com>
2025-08-29 01:17:00 +0200

    modificate app.js and server.js now only one server connect

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,26 @@
+// server.js
+import 'dotenv/config';
 import app from './app.js';
 import connectDB from './config/db.js';
 import clearPort from './utils/clearPort.js';
 import setupShutdownHandler from './utils/shutdownHandler.js';
 
-connectDB();
-
 const PORT =
   process.env.PORT || (process.env.NODE_ENV === 'test' ? 5001 : 5000);
 
-clearPort(PORT);
+let server;
+try {
+  await connectDB(process.env.MONGODB_URI); // дождались БД
+  await clearPort(PORT);
 
-let server = null;
-
-if (process.env.NODE_ENV !== 'test') {
   server = app.listen(PORT, () => {
-    /*   console.log(`🚀 Server started on port ${PORT}`); */
     console.log(`🚀 Server running at: http://localhost:${PORT}`);
   });
 
   setupShutdownHandler(server);
+} catch (err) {
+  console.error('💥 Startup failed:', err.message);
+  process.exit(1);
 }
 
 export default server;
