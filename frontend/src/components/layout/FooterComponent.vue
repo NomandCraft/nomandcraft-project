c3c801fa522d9d18e826626b344aa22587b83c8e
SharmarAndrey <sharmarandrey@gmail.com>
2025-08-30 01:41:30 +0200

    components/layout/FooterComponent.vue

---
+++ frontend/src/components/layout/FooterComponent.vue
@@ -0,0 +1,10 @@
+<template>
+  <footer
+    class="border-t"
+    style="margin-top: 40px; background: #f6f7f8; color: #667"
+  >
+    <div class="container" style="padding: 20px 0; text-align: center">
+      © 2024 Nomadcraft. All rights reserved.
+    </div>
+  </footer>
+</template>
