0932548d7fb58e87898c84e53a7bdc7687d54b45
SharmarAndrey <sharmarandrey@gmail.com>
2025-08-30 01:42:06 +0200

    components/layout/LayoutComponent.vue

---
+++ frontend/src/components/layout/LayoutComponent.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Header from "./HeaderComponent.vue";
+import Footer from "./FooterComponent.vue";
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <Header />
+  <main
+    class="container"
+    style="min-height: calc(100vh - 64px - 80px); padding: 24px 0"
+  >
+    <RouterView />
+  </main>
+  <Footer />
+</template>
